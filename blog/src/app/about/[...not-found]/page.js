e2281352ecebd371eccd15a7d3d6c19c20a2686c
luckkalpna <luckkalpna@gmail.com>
2025-07-04 20:56:26 +0530

    Create Global 404 Page & Routing based 404 Page

---
+++ blog/src/app/about/[...not-found]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutPageNotFound() {
+  return (
+    <div>
+      <h1>This is our about us 404 page</h1>
+    </div>
+  )
+}
---
+++ blog/src/app/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>This is our global 404 page</h1>
+    </div>
+  )
+}
